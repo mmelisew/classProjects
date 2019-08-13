6e0d75d35eec8dddf3367a1f68f0f1589c996fd0
Melisew M <40158089+mmelisew@users.noreply.github.com>
2019-08-12 20:16:08 -0500

    Add files via upload
    Software Engineering, Database and Client side web development class projects

---
+++ Software Enginering project Files and codes/db.php
@@ -0,0 +1,8 @@
+<?php
+
+$con = mysqli_connect("localhost","cs319_1_spr2019_group5_db","cs319Th@@Uj","cs319_1_spr2019_group5_db");
+	if (mysqli_connect_errno()){
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		die();
+		}
+?>
